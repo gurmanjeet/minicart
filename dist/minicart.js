bddd44f1ede5f4bd263050f2cd209cca2cec3497
gurmanjeet <43558637+gurmanjeet@users.noreply.github.com>
2018-09-25 13:28:19 -0400

    Update minicart.js

--- dist/minicart.js
+++ dist/minicart.js
@@ -1915,6 +1915,7 @@ var defaults = module.exports = {
 
     strings: {
         button: 'Check Out with <img src="//cdnjs.cloudflare.com/ajax/libs/minicart/3.0.1/paypal_65x18.png" width="65" height="18" alt="PayPal" />',
+         button: 'Continue Shopping',
         subtotal: 'Subtotal:',
         discount: 'Discount:',
         empty: 'Your shopping cart is empty'
